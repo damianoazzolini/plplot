6ec28e3f4efc41771cdea6b57288ec34ffce20a0
Damiano Azzolini <damiano.azzolini@unife.it>
2019-04-24 15:11:22 +0200

    removed files

--- Makefile
+++
@@ -1,7 +0,0 @@
-SHELL := /bin/bash
-CC=gcc
-
-CFLAGSBDDEM= $(CFLAGS) -fPIC -DBP_FREE -O3 -fomit-frame-pointer -Wall -g -O2 -Wextra ${INCDIRS}
-
-prognuplot.o : prognuplot.c
-	$(CC) -c $(CFLAGSBDDEM) prognuplot.c -o prognuplot.o
--- installer.sh
+++
@@ -1,9 +0,0 @@
-# commands for ubuntu
-# prerequisites
-# sudo apt install libwxgtk3.0-dev
-# sudo apt install libpango1.0-dev
-# sudo apt install libreadline5-dev
-
-gcc -I/usr/local/lib/swipl/include/ -fpic -c plplot.c &&
-# gcc -I/usr/lib/swi-prolog/include/ -fpic -c plplot.c &&
-gcc -shared -o plplot.so plplot.o
